594ea4a94d1053dd881648995551f29536215565
Daniel Osman <daniel.osman@damega.net>
2018-03-13 22:14:40 +0100

    Initial commit.

---
+++ src/index.js
@@ -0,0 +1,96 @@
+import { lineString } from '@turf/helpers'
+import lineOffset from '@turf/line-offset'
+import lineToPolygon from '@turf/line-to-polygon'
+import * as d3Shape from 'd3-shape'
+import simplepolygon from 'simplepolygon'
+import PolygonOffset from 'polygon-offset'
+import xs from 'xstream'
+import fromEvent from 'xstream/extra/fromEvent'
+
+const canvas = document.getElementById('canvas')
+const canvasRect = canvas.getBoundingClientRect()
+const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
+svg.setAttribute('width', canvasRect.width)
+svg.setAttribute('height', canvasRect.height)
+
+let state = 'ready'
+
+const cancelButton = document.getElementById('cancelButton')
+const addIntersectionButton = document.getElementById('addIntersectionButton')
+
+const cancelClick$ = fromEvent(cancelButton, 'click')
+const addIntersectionClick$ = fromEvent(addIntersectionButton, 'click')
+  .map(() => state)
+  .filter(s => s !== 'addIntersection')
+const svgClick$ = fromEvent(svg, 'click')
+
+addIntersectionClick$.addListener({
+  next() {
+    state = 'addIntersection'
+    addIntersectionButton.setAttribute('class', 'active')
+    svgClick$
+      .take(1)
+      .endWhen(cancelClick$)
+      .addListener({
+        next: (ev) => {
+          console.log('svg clicked: ', ev)
+        },
+        complete: () => {
+          addIntersectionButton.setAttribute('class', 'ready')
+          state = 'ready'
+        }
+      })
+  }
+})
+
+
+
+const line = lineString([[10, 10], [100, 10], [100, 12], [20, 30], [80, 80]])
+const offsetPos = lineOffset(line, 5, { units: 'degrees' })
+const offsetNeg = lineOffset(line, -5, { units: 'degrees' })
+const lineSvg = d3Shape.line()
+
+const linePathElem = document.createElementNS('http://www.w3.org/2000/svg', 'path')
+const offsetPosPathElem = document.createElementNS('http://www.w3.org/2000/svg', 'path')
+const offsetNegPathElem = document.createElementNS('http://www.w3.org/2000/svg', 'path')
+const polyPath = document.createElementNS('http://www.w3.org/2000/svg', 'path')
+canvas.appendChild(svg)
+svg.appendChild(linePathElem)
+svg.appendChild(offsetPosPathElem)
+svg.appendChild(offsetNegPathElem)
+svg.appendChild(polyPath)
+
+linePathElem.setAttribute('d', lineSvg(line.geometry.coordinates))
+linePathElem.setAttribute('class', 'base')
+//offsetPosPathElem.setAttribute('d', lineSvg(offsetPos.geometry.coordinates))
+//offsetPosPathElem.setAttribute('class', 'offset')
+
+// Simplyfiy
+/*
+const offsetNegSimple = simplepolygon(lineToPolygon(offsetNeg))
+console.log('offsetNegSimple: ', offsetNegSimple)
+
+offsetNegPathElem.setAttribute('d', lineSvg(offsetNeg.geometry.coordinates))
+offsetNegPathElem.setAttribute('class', 'offset')
+*/
+
+// Polygon offset
+const polygonOffset = new PolygonOffset()
+console.log(polygonOffset, line.geometry.coordinates)
+const offsetNeg2Points = polygonOffset.data(line.geometry.coordinates).arcSegments(5).offsetLine(5)
+console.log(offsetNeg2Points)
+const offsetNeg2 = lineString(offsetNeg2Points[0])
+offsetNegPathElem.setAttribute('d', lineSvg(offsetNeg2.geometry.coordinates))
+offsetNegPathElem.setAttribute('class', 'offset')
+
+// Join line strings.
+/*
+const reversed = offset1.geometry.coordinates.slice(0).reverse()
+const joined = offset.geometry.coordinates.concat(reversed)
+const poly = lineToPolygon(lineString(joined))
+
+polyPath.setAttribute('d', lineSvg(poly.geometry.coordinates[0]))
+polyPath.setAttribute('class', 'polygon')
+
+console.log(poly)
+*/
